2a4b5715303250eb612cd3735c4ba4a6658c4f60
増田功祐 <masudaisaoyuu@kmasuda.local>
2021-06-06 20:30:45 +0900

    210606

---
+++ src/App?.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/headermusic.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer"> 
+        <div class="footercon">@kosuke masuda</div>
+    </footer>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Amatic+SC:700');
+    .footer{
+        padding: 30px;
+        background-color: #BDC0BA;
+    }
+    .footercon{
+        text-align: center;
+        font-family: 'Amatic SC', cursive;
+        font-size: 20px;
+    }
+</style>
